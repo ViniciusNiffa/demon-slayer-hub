c990b7391bd9ae75aad0e350ce322e5d6a007fa4
Vinicius Niffa <viniciusfn0611@gmail.com>
2026-05-23 14:28:50 -0300

    Remove config.py do rastreamento e atualiza o gitignore

--- config.py
+++
@@ -1,4 +0,0 @@
-SECRET_KEY = "ÇÇ^^^^^^````29329389!¨&&(#!¨!#&*¨#*&¨#!!NSADJIANSDI!N18IU9iah9ushh9128u032@##!¨&asofjao(&@&#sfaoijhfoa&@¨Y&#!¨&*#%*¨#%6¨*&#%¨*&%#¨&ÇÇÇÇ)"
-DATABASE = "database.db"
-UPLOAD_FOLDER = "static/uploads"
-DEFAULT_IMAGE = "default.png"
